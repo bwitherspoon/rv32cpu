a98c361e5dad1b2e292836c8189d35042e40fef6
Brett Witherspoon <spoonb@cdspooner.com>
2015-10-29 12:49:32 -0500

    regfile: rename ports

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -9,15 +9,15 @@ module regfile #(
 )(
     input  logic                  clk,
 
-    input  logic [ADDR_WIDTH-1:0] rs1_addr,
-    output logic [DATA_WIDTH-1:0] rs1_data,
+    input  logic [ADDR_WIDTH-1:0] raddr1,
+    output logic [DATA_WIDTH-1:0] rdata1,
 
-    input  logic [ADDR_WIDTH-1:0] rs2_addr,
-    output logic [DATA_WIDTH-1:0] rs2_data,
+    input  logic [ADDR_WIDTH-1:0] raddr2,
+    output logic [DATA_WIDTH-1:0] rdata2,
 
-    input  logic                  rd_wen,
-    input  logic [ADDR_WIDTH-1:0] rd_addr,
-    input  logic [DATA_WIDTH-1:0] rd_data
+    input  logic                  wen,
+    input  logic [ADDR_WIDTH-1:0] waddr,
+    input  logic [DATA_WIDTH-1:0] wdata
 );
 
     logic [DATA_WIDTH-1:0] regs [0:2**ADDR_WIDTH-2];
@@ -26,15 +26,15 @@ module regfile #(
     initial for (int i = 0; i < 2**ADDR_WIDTH-1; i = i + 1) regs[i] = 0;
 `endif
     
-    logic rs1_zero = rs1_addr == {ADDR_WIDTH{1'b0}};
-    logic rs2_zero = rs2_addr == {ADDR_WIDTH{1'b0}};
-    logic rd_zero  = rd_addr  == {ADDR_WIDTH{1'b0}};
+    logic rzero1 = raddr1 == {ADDR_WIDTH{1'b0}};
+    logic rzero2 = raddr2 == {ADDR_WIDTH{1'b0}};
+    logic wzero  = waddr  == {ADDR_WIDTH{1'b0}};
 
     always @(negedge clk)
-        if (rd_wen && ~rd_zero)
-            regs[rd_addr - 1] <= rd_data;
+        if (wen && ~wzero)
+            regs[waddr - 1] <= wdata;
 
-    assign rs1_data = rs1_zero ? {DATA_WIDTH{1'b0}} : regs[rs1_addr - 1];
-    assign rs2_data = rs2_zero ? {DATA_WIDTH{1'b0}} : regs[rs2_addr - 1];
+    assign rdata1 = rzero1 ? {DATA_WIDTH{1'b0}} : regs[raddr1 - 1];
+    assign rdata2 = rzero2 ? {DATA_WIDTH{1'b0}} : regs[raddr2 - 1];
 
 endmodule
