c315fd99cc7eb31728a53884daabed5c809532c1
Brett Witherspoon <spoonb@cdspooner.com>
2015-10-29 15:10:23 -0500

    add riscv package

---
+++ rtl/riscv.sv
@@ -0,0 +1,14 @@
+/**
+ * Package: riscv
+ */
+package riscv;
+
+    localparam WORD_WIDTH = 32;
+    localparam REGS_ADDR_WIDTH = 5;
+    localparam REGS_DATA_WIDTH = WORD_WIDTH;
+    localparam IMEM_ADDR_WIDTH = 9;
+    localparam IMEM_DATA_WIDTH = WORD_WIDTH;
+    
+endpackage
+
+
