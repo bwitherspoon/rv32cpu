977e05a16b9c02cf2c07e2da93be60f558e2a298
Brett Witherspoon <spoonb@cdspooner.com>
2015-10-29 12:51:42 -0500

    core: add incomplete test

---
+++ sim/core/core_test.sv
@@ -0,0 +1,26 @@
+module core_test;
+  
+    timeunit 1ns;
+    timeprecision 1ps;
+    
+    localparam CLOCK_PERIOD = 100;
+  
+      // Clock
+    bit clk = 0;
+    initial forever #(CLOCK_PERIOD/2) clk = ~clk;
+
+    // Reset
+    bit resetn = 1;
+    task reset();
+        @(posedge clk) #1 resetn = 0;
+        @(posedge clk) #1 resetn = 1;
+        @(posedge clk) #1;
+    endtask
+
+    core core(.*);
+    
+    initial $finish;
+    
+endmodule
+
+
