c29d45b2da297789993fe42036aa969c38529279
Brett Witherspoon <spoonb@cdspooner.com>
2015-10-31 12:07:33 -0500

    regfile: simplify and prevent out of range access

--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -24,25 +24,19 @@ module regfile (
     input  word_t wdata
 );
 
-    localparam ADDR_WIDTH = $bits(addr_t);
-
-    word_t regs [0:2**ADDR_WIDTH-2];
+    word_t regs [0:2**$bits(addr_t)-1];
 
 `ifndef SYNTHESIS
     initial
-        for (int i = 0; i < 2**ADDR_WIDTH-1; i = i + 1)
+        for (int i = 0; i < 2**$bits(addr_t)-1; i = i + 1)
             regs[i] = 0;
 `endif
 
-    logic rzero1 = raddr1 == 'b0;
-    logic rzero2 = raddr2 == 'b0;
-    logic wzero  = waddr  == 'b0;
-
     always @(negedge clk)
-        if (wen && ~wzero)
-            regs[waddr - 1] <= wdata;
+        if (wen && waddr != 'd0)
+            regs[waddr] <= wdata;
 
-    assign rdata1 = rzero1 ? 'b0 : regs[raddr1 - 1];
-    assign rdata2 = rzero2 ? 'b0 : regs[raddr2 - 1];
+    assign rdata1 = raddr1 == 'd0 ? 'd0 : regs[raddr1];
+    assign rdata2 = raddr2 == 'd0 ? 'd0 : regs[raddr2];
 
 endmodule
