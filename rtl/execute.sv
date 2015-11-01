2d4c4d801311084ba7ca8d10f8f3fd7d0d2bfe1f
Brett Witherspoon <spoonb@cdspooner.com>
2015-11-01 10:03:58 -0600

    execute: update ports

--- rtl/execute.sv
+++ rtl/execute.sv
@@ -1,21 +1,34 @@
+/*
+ * execute.sv
+ */
+ 
+import riscv::pc_t;
+import riscv::funct_t;
+import riscv::word_t;
+ 
 /**
  * Module: execute
  */
 module execute (
-    input  logic         clk,
-    input  riscv::pc_t   pc,
-    output riscv::word_t bypass_alu,
-    output riscv::word_t out,
-    output riscv::pc_t   jal_bxx_tgt,
-    output riscv::pc_t   jalr_tgt
+    input  logic   clk,
+    input  pc_t    pc,
+    input  funct_t funct,
+    input  word_t  op1,
+    input  word_t  op2,
+    output word_t  out,
+    output pc_t    jal_bxx_tgt,
+    output pc_t    jalr_tgt,
+    output logic   eq,
+    output logic   lt,
+    output logic   ltu
 );
 
     alu alu (
-        .funct(),
+        .funct(funct),
         .shamt(),
-        .op1(),
-        .op2(),
-        .out()
+        .op1(op1),
+        .op2(op2),
+        .out(out)
     );
 
 endmodule
