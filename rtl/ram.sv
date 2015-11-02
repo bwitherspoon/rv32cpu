937463cea9c4ef630e44601fb83ac40da37621e6
Brett Witherspoon <spoonb@cdspooner.com>
2015-11-02 00:35:13 -0600

    memory: add ram

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -5,35 +5,26 @@
 import riscv::addr_t;
 import riscv::data_t;
 
+typedef logic [$bits(data_t)/8-1:0] en_t;
+
 /**
  * Module: ram
  */
 module ram (
-    input  logic                  clk,
-    // Port A
-    input  logic [DATA_WIDTH/8-1:0] wen,
-    input  logic [ADDR_WIDTH-1:0]   addr,
-    input  logic [DATA_WIDTH-1:0]   wdata,
-    output logic [DATA_WIDTH-1:0]   rdata,
+    input  logic  clk,
+    input  en_t   wen,
+    input  addr_t addr,
+    input  data_t wdata,
+    output data_t rdata,
 );
-    localparam DATA_BYTES = DATA_WIDTH / 8;
 
-    logic [DATA_WIDTH-1:0] ram [0:2**ADDR_WIDTH-1];
+    logic [7:0] ram [0:2**$bits(addr_t)-1];
 
-    // Port A (with byte write enable)
-    always_ff @(posedge clk) begin
-        for (int i = 0; i < DATA_BYTES; i = i + 1)
-            if (we_a[i])
-                ram[addr_a][8*i +: 8] <= wdata_a[8*i +: 8];
-        rdata_a <= ram[addr_a];
-    end
-
-    // Port A (with byte write enable)
-    always_ff @(posedge clk) begin
-        for (int i = 0; i < DATA_BYTES; i = i + 1)
-            if (we_b[i])
-                ram[addr_b][8*i +: 8] <= wdata_b[8*i +: 8];
-        rdata_b <= ram[addr_b];
-    end
+    always_ff @(posedge clk)
+        for (int i = 0; i < $bits(data_t)/8; i = i + 1) begin
+            if (wen[i])
+                ram[addr + i] <= wdata[8*i +: 8];
+            rdata[8*i +: 8] <= ram[addr + i];
+        end
 
 endmodule
