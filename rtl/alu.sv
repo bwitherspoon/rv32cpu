33c50f2dd425b610dcd9b9991b85a0e6c03c103e
Brett Witherspoon <spoonb@cdspooner.com>
2015-11-01 23:55:43 -0600

    alu: slice shamt

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -16,16 +16,18 @@ module alu (
     input  data_t  op2,
     output data_t  out
 );
+    logic [6:0] shamt = op2[5:0];
+
     always_comb
         unique case (opcode)
             aluop::ADD:  out = op1 + op2;
             aluop::SUB:  out = op1 - op2;
-            aluop::SLL:  out = op1 << op2;
+            aluop::SLL:  out = op1 << shamt;
             aluop::SLT:  out = signed'(op1) < signed'(op2);
             aluop::SLTU: out = op1 < op2;
             aluop::XOR:  out = op1 ^ op2;
-            aluop::SRL:  out = op1 >> op2;
-            aluop::SRA:  out = signed'(op1) >>> op2;
+            aluop::SRL:  out = op1 >> shamt;
+            aluop::SRA:  out = signed'(op1) >>> shamt;
             aluop::OR:   out = op1 | op2;
             aluop::AND:  out = op1 & op2;
         endcase
