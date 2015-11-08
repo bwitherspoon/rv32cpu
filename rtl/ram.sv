f04d5a4b213d2670f89dadc4e831c2e6ac6b90d9
Brett Witherspoon <spoonb@cdspooner.com>
2015-11-08 07:47:49 -0600

    ram: fixup

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -9,19 +9,21 @@ typedef logic [$bits(data_t)/8-1:0] en_t;
 
 /**
  * Module: ram
+ *
+ * Byte addressable synchronous RAM
  */
 module ram (
     input  logic  clk,
     input  en_t   wen,
     input  addr_t addr,
     input  data_t wdata,
-    output data_t rdata,
+    output data_t rdata
 );
 
     logic [7:0] ram [0:2**$bits(addr_t)-1];
 
     always_ff @(posedge clk)
-        for (int i = 0; i < $bits(data_t)/8; i = i + 1) begin
+        for (int i = 0; i < $bits(data_t)/8; i++) begin
             if (wen[i])
                 ram[addr + i] <= wdata[8*i +: 8];
             rdata[8*i +: 8] <= ram[addr + i];
