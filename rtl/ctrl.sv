f494c0ded6ada758f90594c4e9439abd1bf3b79b
Brett Witherspoon <spoonb@cdspooner.com>
2015-12-16 16:44:35 -0600

    ctrl: cleanup

--- rtl/ctrl.sv
+++ rtl/ctrl.sv
@@ -5,179 +5,84 @@ package ctrl;
 
     import aluop::aluop_t;
 
+    // Program counter select
+    typedef enum logic [1:0] {
+        PC_TARGET,
+        PC_TRAP,
+        PC_NEXT
+    } pc_sel_t;
+
     // Operand select
     typedef enum logic {
-        RS1,
-        PC,
-        X = 'x
+        OP1_RS1,
+        OP1_PC,
+        OP1_DONTCARE = 'x
     } op1_sel_t;
 
     // Operand select
     typedef enum logic [2:0] {
-        RS2,
-        I_IMM,
-        S_IMM,
-        B_IMM,
-        U_IMM,
-        J_IMM,
-        FOUR,
-        XXX = 'x
+        OP2_RS2,
+        OP2_I_IMM,
+        OP2_S_IMM,
+        OP2_B_IMM,
+        OP2_U_IMM,
+        OP2_J_IMM,
+        OP2_DONTCARE = 'x
     } op2_sel_t;
 
+    // Execute stage output select
+    typedef enum logic {
+        OUT_ALU,
+        OUT_PC_PLUS4,
+        OUT_DONTCARE = 'x
+    } out_sel_t;
+
+    // Memeory stage output select
+    typedef enum logic {
+        DAT_ALU,
+        DAT_MEM,
+        DAT_DONTCARE = 'x
+    } dat_sel_t;
+
     typedef struct {
-        logic invalid;
-        logic bubble;
-        logic jump;
         logic load;
         logic store;
-        logic register;
-        aluop_t operation;
+        logic write;
+        aluop_t funct;
+        pc_sel_t pc_sel;
         op1_sel_t op1_sel;
         op2_sel_t op2_sel;
+        dat_sel_t dat_sel;
     } ctrl_t;
 
     localparam ctrl_t INVALID = '{
-        invalid:   '1,
-        bubble:    '0,
-        jump:      '1,
-        load:      '0,
-        store:     '0,
-        register:  '0,
-        operation: aluop::XXX,
-        op1_sel:   X,
-        op2_sel:   XXX
+        load:    'x,
+        store:   '0,
+        write:   '0,
+        funct:   aluop::XXX,
+        pc_sel:  PC_TRAP,
+        op1_sel: OP1_DONTCARE,
+        op2_sel: OP2_DONTCARE,
+        dat_sel: DAT_DONTCARE
+    };
+    localparam ctrl_t NOP = '{
+        load:    '0,
+        store:   '0,
+        write:   '0,
+        funct:   aluop::XXX,
+        pc_sel:  PC_NEXT,
+        op1_sel: OP1_DONTCARE,
+        op2_sel: OP2_DONTCARE,
+        dat_sel: DAT_DONTCARE
     };
     localparam ctrl_t ADDI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::ADD,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
+        load:    '0,
+        store:   '0,
+        write:   '1,
+        funct:   aluop::ADD,
+        pc_sel:  PC_NEXT,
+        op1_sel: OP1_RS1,
+        op2_sel: OP2_I_IMM,
+        dat_sel: DAT_ALU
     };
-    localparam ctrl_t SLTI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::SLT,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t SLTIU = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::SLTU,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t ANDI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::AND,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t ORI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::OR,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t XORI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::XOR,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t SLLI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::SLL,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t SRLI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::SRL,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t SRAI = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::SRA,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t AUIPC = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '0,
-        register:  '1,
-        operation: aluop::ADD,
-        op1_sel:   PC,
-        op2_sel:   U_IMM
-    };
-    localparam ctrl_t LW = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '1,
-        store:     '0,
-        register:  '1,
-        operation: aluop::ADD,
-        op1_sel:   RS1,
-        op2_sel:   I_IMM
-    };
-    localparam ctrl_t SW = '{
-        invalid:   '0,
-        bubble:    '0,
-        jump:      '0,
-        load:      '0,
-        store:     '1,
-        register:  '0,
-        operation: aluop::ADD,
-        op1_sel:   RS1,
-        op2_sel:   S_IMM
-    };
-
 endpackage
