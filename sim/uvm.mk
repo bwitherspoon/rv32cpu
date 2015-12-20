b12c4d5c2d72eba87d44477fe53a3df6cf19fa93
Brett Witherspoon <spoonb@cdspooner.com>
2015-12-19 19:13:32 -0600

    sim: remove uvm makefile

--- sim/uvm.mk
+++
@@ -1,9 +0,0 @@
-SRC += $(UVM_HOME)/src/uvm.sv
-
-VLOG_OPT = +incdir+$(UVM_HOME)/src \
-           +define+UVM_NO_DEPRECATED \
-           +define+UVM_OBJECT_DO_NOT_NEED_CONSTRUCTOR
-
-VSIM_OPT = -sv_lib $(UVM_HOME)/lib/uvm_dpi \
-
-include ../common.mk
