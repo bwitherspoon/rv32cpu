dbf7137642a2047c3ffa20eb5ed96c10a400d2d8
Brett Witherspoon <spoonb@cdspooner.com>
2015-12-22 18:56:17 -0600

    sim: cleanup top

--- sim/core/top.sv
+++ sim/core/top.sv
@@ -23,19 +23,11 @@ module top;
     core core(.*);
 
     initial begin
-        $readmemh("main.txt", core.memory.ram.mem);
         $dumpfile("top.vcd");
         $dumpvars();
 
-        $monitor("%6g: t0=%H,t1=%H,t2=%H,t3=%H,t4=%H,t5=%H,M[128]=%H", $time,
-                 core.regfile.regs[5], core.regfile.regs[6],
-                 core.regfile.regs[7], core.regfile.regs[28],
-                 core.regfile.regs[29], core.regfile.regs[30],
-                 core.memory.ram.mem[128]);
-
-
         reset();
-        repeat (30) @(posedge clk);
+        repeat (15) @(posedge clk);
         $finish;
     end
 
