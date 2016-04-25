cc4b40e25d3b00ff27dfe7a60c02f13fa0dc19fe
Brett Witherspoon <spoonb@cdspooner.com>
2016-04-25 12:22:35 -0500

    asm: add new serial boot loader

---
+++ src/loader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+import struct
+
+import serial
+
+def main():
+    parser = argparse.ArgumentParser(description='Program loader')
+    parser.add_argument('-f', '--file', required=True, help='set the input file')
+    parser.add_argument('-p', '--port', required=True, help='set the output port')
+
+    args = parser.parse_args()
+
+    dev = serial.Serial(args.port)
+
+    with open(args.file, 'r') as fd:
+        data = fd.read()
+        size = len(data) / 4
+        dev.write(struct.pack('>H', size))
+        words = [data[i:i+4] for i in range(0, len(data), 4)]
+        for word in words:
+            dev.write(word[::-1])
+
+    dev.close()
+
+if __name__ == "__main__":
+    main()
